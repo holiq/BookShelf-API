199a1ad2c9341beaa62c5d642bc2dbf98c0c8395
holiq <holiq.ibrahim376@gmail.com>
2023-05-15 08:26:41 +0700

    feat: list and store

--- src/handler.js
+++ src/handler.js
@@ -6,3 +6,68 @@ export const homeHandler = (request, h) => {
     name: "BookShelf API",
   });
 };
+
+export const listBooksHandler = (request, h) => {
+  return h.response({
+    status: "success",
+    data: {
+      books: books,
+    },
+  });
+};
+
+export const storeBookHandler = (request, h) => {
+  const id = nanoid(16);
+  const {
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
+  } = request.payload;
+  const insertedAt = new Date().toISOString();
+  const updatedAt = insertedAt;
+  const isFinished = pageCount === readPage ? true : false;
+  if (!name) {
+    return h.response({
+      status: "fail",
+      message: "Gagagl menambahkan buku. Mohon isi nama buku",
+    }).code(400);
+  }
+
+  if (readPage > pageCount) {
+    return h.response({
+      status: "fail",
+      message:
+        "Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount ",
+    }).code(400);
+  }
+
+  const newBook = {
+    id,
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    isFinished,
+    reading,
+    insertedAt,
+    updatedAt,
+  };
+
+  books.push(newBook);
+
+  return h.response({
+    status: "success",
+    message: "Buku berhasil ditambahkan",
+    data: {
+      bookId: id,
+    },
+  }).code(201);
+};
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,19 @@
-import { homeHandler } from "./handler.js";
+import { homeHandler, listBooksHandler, storeBookHandler } from "./handler.js";
 const routes = [
   {
     method: "GET",
     path: "/",
     handler: homeHandler,
+  },
+  {
+    method: "GET",
+    path: "/books",
+    handler: listBooksHandler,
+  },
+  {
+    method: "POST",
+    path: "/books",
+    handler: storeBookHandler,
   },
 ];
 
